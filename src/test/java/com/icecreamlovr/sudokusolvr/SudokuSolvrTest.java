2aaf600a5fa8c62f2fc831da4c9eff4a75d73c5e
Yue He <yuehe.neu@gmail.com>
2022-11-27 22:44:23 -0800

    Add gradle and junit.

---
+++ src/test/java/com/icecreamlovr/sudokusolvr/SudokuSolvrTest.java
@@ -0,0 +1,13 @@
+package com.icecreamlovr.sudokusolvr;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SudokuSolvrTest {
+  @Test
+  public void testBlah() {
+    assertEquals(9, SudokuSolvr.blah(7));
+  }
+
+}
