49fbb36cc8b04167218f98fc6fd6f3a93874c5d3
Yue He <yuehe.neu@gmail.com>
2022-12-18 18:27:09 -0800

    Add a simple sudoku solver using DFS backtracking.

--- src/main/java/com/icecreamlovr/sudokusolvr/SudokuSolvr.java
+++ src/main/java/com/icecreamlovr/sudokusolvr/SudokuSolvr.java
@@ -1,7 +1,144 @@
 package com.icecreamlovr.sudokusolvr;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SudokuSolvr {
-  public static int blah(int input) {
-    return input + 1;
+
+  private final int LENGTH = 9;
+
+  private static class Coords {
+    private int row;
+    private int col;
+
+    public Coords(int row, int col) {
+      this.row = row;
+      this.col = col;
+    }
+
+    @Override
+    public String toString() {
+      return (row + "," + col);
+    }
+  }
+
+  /**
+   * Given a sudoku input, attempt to solve it using DFS + backtracking.
+   * If successful, return the solved board.
+   */
+  public int[][] solveSudoku(int[][] board) {
+    checkBoardSize(board);
+    checkNumberRange(board);
+
+    List<Coords> solvingOrder = getSolvingOrder(board);
+    solveRecur(board, solvingOrder, 0);
+
+    return board;
+  }
+
+  // Verifies that the board size is 9x9.
+  private static void checkBoardSize(int[][] board) {
+    if (board.length != 9) {
+      throw new IllegalArgumentException("Size need to be equal to 9.");
+    }
+    for (int i = 0; i < 9; i++) {
+      if (board[i].length != 9) {
+        throw new IllegalArgumentException("Size need to be equal to 9.");
+      }
+    }
+  }
+
+  // Verifies that the numbers in the input are between 0-9 (0 meaning empty).
+  private static void checkNumberRange(int[][] board) {
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board.length; j++) {
+        if (board[i][j] > 9 || board[i][j] < 0) {
+          throw new IllegalArgumentException("Input numbers need to be between 0 and 9.");
+        }
+      }
+    }
+  }
+
+  // Get an ordered list of coordinates that need to be solved.
+  // The algorithm will follow this order to solve the sudoku.
+  private List<Coords> getSolvingOrder(int[][] board) {
+    List<Coords> order = new ArrayList<>();
+    for (int i = 0; i < LENGTH; i++) {
+      for (int j = 0; j < LENGTH; j++) {
+        if (board[i][j] == 0) {
+          order.add(new Coords(i, j));
+        }
+      }
+    }
+    return order;
+  }
+
+  // Recursively solves the sudoku using DFS + backtracking.
+  // The algorithm will follow a specific order to determine what slot to solve next.
+  // The recursive call returns false if it cannot move forward.
+  private boolean solveRecur(int[][] board, List<Coords> solvingOrder, int orderIndex) {
+    if (orderIndex == solvingOrder.size()) {
+      // Base case: no more slot to solve.
+      return true;
+    }
+
+    Coords boardSlot = solvingOrder.get(orderIndex);
+    List<Integer> guesses = getGuesses(boardSlot, board);
+    if (guesses.isEmpty()) {
+      // Base case: impossible to solve further.
+      return false;
+    }
+
+    for (int guess : guesses) {
+      // Make a guess, then call solve recursively.
+      board[boardSlot.row][boardSlot.col] = guess;
+      if (!solveRecur(board, solvingOrder, orderIndex + 1)) {
+        // If the child solve call returns false, revert the guess.
+        board[boardSlot.row][boardSlot.col] = 0;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // For a given unsolved slot, gets the list of potential numbers it can be.
+  // This is done by going through the row, the column and the 3x3 square this slot is in.
+  private List<Integer> getGuesses(Coords boardSlot, int[][] board) {
+    // Create a boolean array for masking.
+    boolean[] existingNumbers = new boolean[9];
+    int row = boardSlot.row;
+    int col = boardSlot.col;
+
+    // Go through the rows and columns.
+    for (int i = 0; i < LENGTH; i++) {
+      int currentRow = board[row][i];
+      if (currentRow != 0) {
+        existingNumbers[currentRow - 1] = true;
+      }
+      int currentCol = board[i][col];
+      if (currentCol != 0) {
+        existingNumbers[currentCol - 1] = true;
+      }
+    }
+
+    // Go through the 3x3 square.
+    for (int i = row / 3 * 3; i <= row / 3 * 3 + 2; i++) {
+      for (int j = col / 3 * 3; j <= col / 3 * 3 + 2; j++) {
+        int current = board[i][j];
+        if (current != 0) {
+          existingNumbers[current - 1] = true;
+        }
+      }
+    }
+
+    // Transform the boolean mask array to list of numbers.
+    List<Integer> possibleGuesses = new ArrayList<>();
+    for (int i = 0; i < existingNumbers.length; i++) {
+      if (!existingNumbers[i]) {
+        possibleGuesses.add(i + 1);
+      }
+    }
+    return possibleGuesses;
   }
 }
