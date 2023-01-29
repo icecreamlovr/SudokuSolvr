57fb7a6fc2ff937e896fb7429e90dfe3a6a62799
Yue He <yuehe.neu@gmail.com>
2023-01-28 16:23:04 -0800

    Add CLI support which allows passing Sudoku puzzle with --input.

--- src/main/java/com/icecreamlovr/sudokusolvr/cli/SudokuSolvrCli.java
+++ src/main/java/com/icecreamlovr/sudokusolvr/cli/SudokuSolvrCli.java
@@ -1,23 +1,108 @@
 package com.icecreamlovr.sudokusolvr.cli;
 
+import com.icecreamlovr.sudokusolvr.solvr.SudokuSolvr;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.ParseException;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class SudokuSolvrCli {
   public static void main(String[] args) throws ParseException {
-    System.out.println(">>>Hello world CLI!");
-
+    // Define one flag --input
     Options options = new Options();
-    options.addOption("x", false, "random flag");
+    Option input = Option.builder()
+            .longOpt("input")
+            .desc("The input board. Use form [[1,2,3,..],[9,8,7,...],...]")
+            .hasArg()
+            .argName("BOARD")
+            .build();
+    options.addOption(input);
 
+    // Parse the args with the flags defined above.
     CommandLineParser parser = new DefaultParser();
     CommandLine cmd = parser.parse(options, args);
+    String inputBoard;
+    if(cmd.hasOption("input")) {
+      inputBoard = cmd.getOptionValue("input");
+    } else {
+      System.out.println("No input board supplied.");
+      return;
+    }
 
-    if(cmd.hasOption("x")) {
-      System.out.println("Hi!");
+    // Parse the CLI input into the board we can work with.
+    int[][] board = parseBoard(inputBoard);
+
+    // Solve and print the result to STDOUT.
+    System.out.println("Input board:");
+    prettyPrint(board);
+    SudokuSolvr.solveSudoku(board);
+    System.out.println();
+    System.out.println("Solved:");
+    prettyPrint(board);
+  }
+
+  private static void prettyPrint(int[][] board) {
+    for (int[] line : board) {
+      System.out.println(Arrays.toString(line));
+    }
+  }
+
+  // Parse square-brackets-enclosed, comma-separated sudoku board string
+  // into 2 dimensional array.
+  protected static int[][] parseBoard(String inputBoard) throws ParseException {
+    // First remove whitespaces.
+    String sanitized = inputBoard.replaceAll("\\s", "");
+
+    // Parse the board into 9 rows for further processing.
+    // The regex will match "[[row],[row],[row],[row],[row],[row],[row],[row],[row]]",
+    // where row is /[0-9,]+/
+    String singleRow = "\\[([0-9,]+)\\]";
+    String singleRowWithComma = singleRow + ",";
+    Pattern pattern =
+            Pattern.compile(
+                    "^\\["
+                            + singleRowWithComma.repeat(8)
+                            + singleRow
+                            + "\\]$");
+
+    Matcher matcher = pattern.matcher(sanitized);
+
+    if (matcher.find() && matcher.groupCount() == 9) {
+      // Parse each row.
+      int resultBoard[][] = new int[9][9];
+
+      for (int i = 0; i < 9; i++) {
+        // matcher.group(i) is 1-indexed (group(0) is the matched string itself).
+        int[] parsedRow = parseRow(matcher.group(i + 1));
+        resultBoard[i] = parsedRow;
+      }
+
+      return resultBoard;
+    } else {
+      throw new ParseException("Not matched.");
+    }
+  }
+
+  private static int[] parseRow(String oneRowInput) throws ParseException {
+    Pattern pattern =
+            Pattern.compile(
+                    "([0-9]),([0-9]),([0-9]),([0-9]),([0-9]),([0-9]),([0-9]),([0-9]),([0-9])");
+    Matcher matcher = pattern.matcher(oneRowInput);
+
+    if (matcher.find() && matcher.groupCount() == 9) {
+      int[] resultRow = new int[9];
+      for (int i = 0; i < 9; i++) {
+        // matcher.group(i) is 1-indexed (group(0) is the matched string itself).
+        resultRow[i] = Integer.parseInt(matcher.group(i + 1));
+      }
+      return resultRow;
+    } else {
+      throw new ParseException("Not matched: " + oneRowInput);
     }
   }
 }
--- src/test/java/com/icecreamlovr/sudokusolvr/cli/SudokuSolvrCliTest.java
+++ src/test/java/com/icecreamlovr/sudokusolvr/cli/SudokuSolvrCliTest.java
@@ -1,7 +1,89 @@
 package com.icecreamlovr.sudokusolvr.cli;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.apache.commons.cli.ParseException;
+import org.junit.jupiter.api.Test;
 
 class SudokuSolvrCliTest {
+  @Test
+  public void testParseBoardSuccess() throws ParseException {
+    String board = "[[1,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]]";
+    int[][] expected = new int[9][9];
+    expected[0][0] = 1;
+    assertThat(SudokuSolvrCli.parseBoard(board))
+          .isEqualTo(expected);
+  }
 
+  @Test
+  public void testParseBoardRemovesWhitespace() throws ParseException {
+    String board = "[[2,0 ,0,0,0,0,0,0,0],\n" +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]]";
+    int[][] expected = new int[9][9];
+    expected[0][0] = 2;
+    assertThat(SudokuSolvrCli.parseBoard(board))
+            .isEqualTo(expected);
+  }
+
+  @Test
+  public void testParseBoardFailure() throws ParseException {
+    // incorrect number of lines
+    String board1 = "[[1,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]]";
+    ParseException actualException1 = assertThrows(
+            ParseException.class, () -> SudokuSolvrCli.parseBoard(board1));
+    assertThat(actualException1.getMessage()).isEqualTo("Not matched.");
+
+    // missing []
+    String board2 = "[[1,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]]";
+    ParseException actualException2 = assertThrows(
+            ParseException.class, () -> SudokuSolvrCli.parseBoard(board2));
+    assertThat(actualException2.getMessage()).isEqualTo("Not matched.");
+
+    // Numbers are wrong
+    String board3 = "[[1,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,30,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]," +
+            "[0,0,0,0,0,0,0,0,0]]";
+    ParseException actualException3 = assertThrows(
+            ParseException.class, () -> SudokuSolvrCli.parseBoard(board3));
+    assertThat(actualException3.getMessage()).isEqualTo("Not matched: 0,0,0,0,30,0,0,0");
+  }
 }
