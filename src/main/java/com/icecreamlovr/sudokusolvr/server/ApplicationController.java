301677ba744edb629a92f9b1d7f539255d39511c
Yue He <yuehe.neu@gmail.com>
2023-01-28 20:38:53 -0800

    Serve simple Sudoku page using Thymeleaf templating.

---
+++ src/main/java/com/icecreamlovr/sudokusolvr/server/ApplicationController.java
@@ -0,0 +1,29 @@
+package com.icecreamlovr.sudokusolvr.server;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ApplicationController {
+
+  @GetMapping("/")
+  public String index(
+          @RequestParam(name="difficulty", defaultValue="easy") String difficulty,
+          Model model) {
+    int[][] board = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}};
+    model.addAttribute("board", board);
+    model.addAttribute("difficulty", difficulty);
+    return "index";
+  }
+}
