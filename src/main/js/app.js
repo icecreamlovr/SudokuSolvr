e81bc01c0c3ab88b9cb04e4804e1a0712ed12230
Yue He <yuehe.neu@gmail.com>
2023-01-30 00:25:56 -0800

    Draw a single tile in the Sudoku board using React.

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,17 +1,50 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 
-class Main extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello world! Using React.js</h1>
-            </div>
-        );
+class Tile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+  }
+
+  handleInput = (event) => {
+    event.preventDefault();
+    const filteredValue = event.target.value.replace(/[^1-9]/g, '');
+    if (filteredValue != "") {
+      // So that invalid inputs (e.g. alphabeticals) doesn't clear the tile
+      this.setState({ value: filteredValue });
+    } else if (event.target.value == "") {
+      // So that delete (backspace) can clear the tile.
+      // Note this checks the original input value, not the filtered one.
+      this.setState({ value: filteredValue });
     }
+  }
+
+  render() {
+    return (
+      <input
+        className="tile"
+        type="text"
+        maxLength="1"
+        onInput={this.handleInput}
+        value={this.state.value} />
+    );
+  }
 }
 
-ReactDOM.render(
-    <Main />,
-    document.getElementById('react-mountpoint')
-);
+class Board extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          <Tile value="7" />
+        </div>
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("react-mountpoint"));
+root.render(<Board />);
